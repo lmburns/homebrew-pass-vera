eb082f5ed9b55cb897fa9c51c777846141aab934
Lucas Burns <burnsac@me.com>
2021-04-03 14:51:50 -0500

    v1.0

--- Formula/pass-vera.rb
+++ Formula/pass-vera.rb
@@ -3,7 +3,7 @@ class PassVera < Formula
   homepage "https://github.com/lmburns/pass-vera"
   url "https://github.com/lmburns/pass-vera/archive/refs/tags/v1.0.tar.gz"
   version "v1.0"
-  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+  sha256 "74dca41a95de2c92c8544e4465327d80cba69bee0144860063dc227d0f187773"
   license "GPL-3.0"
   head "https://github.com/lmburns/pass-vera.git"
 
