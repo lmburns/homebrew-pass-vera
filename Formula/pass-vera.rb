52c3bc0c5272aea2c715dce066b69f2358a7a768
Lucas Burns <burnsac@me.com>
2021-05-15 21:52:48 -0500

    v2.1

--- Formula/pass-vera.rb
+++ Formula/pass-vera.rb
@@ -1,15 +1,18 @@
 class PassVera < Formula
   desc "Pass extension that encrypts the password-store in a VeraCrypt drive"
   homepage "https://github.com/lmburns/pass-vera"
-  url "https://github.com/lmburns/pass-vera/archive/refs/tags/v2.0.tar.gz"
-  version "v2.0"
-  sha256 "4f55a8b3e907d7a169bf3c67cd77da51a082f7e65382de484b6b3cd12bca5f8b"
+  url "https://github.com/lmburns/pass-vera/archive/refs/tags/v2.1.tar.gz"
+  version "v2.1"
+  sha256 "6ad02d99ef1a637382b5f3cff5d1a888f16bc1794591a70249bd12b37b45dcfb"
   license "GPL-3.0"
   head "https://github.com/lmburns/pass-vera.git"
 
   depends_on "pass"
   depends_on "coreutils"
+  depends_on "moreutils"
   depends_on "ripgrep"
+  depends_on "jq" => :optional
+  depends_on "yq" => :optional
   depends_on "lolcat" => :optional
   depends_on "figlet" => :optional
   depends_on "toilet" => :optional
